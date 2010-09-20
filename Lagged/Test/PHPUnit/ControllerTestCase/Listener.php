b1c9e13a17e24c1b26afe12b591462301e14bb82
Till Klampaeckel <till@php.net>
2010-09-20 03:33:35 +0200

    * initial commit

---
+++ Lagged/Test/PHPUnit/ControllerTestCase/Listener.php
@@ -0,0 +1,144 @@
+<?php
+/**
+ * +-----------------------------------------------------------------------+
+ * | Copyright (c) 2010, Till Klampaeckel                                  |
+ * | All rights reserved.                                                  |
+ * |                                                                       |
+ * | Redistribution and use in source and binary forms, with or without    |
+ * | modification, are permitted provided that the following conditions    |
+ * | are met:                                                              |
+ * |                                                                       |
+ * | o Redistributions of source code must retain the above copyright      |
+ * |   notice, this list of conditions and the following disclaimer.       |
+ * | o Redistributions in binary form must reproduce the above copyright   |
+ * |   notice, this list of conditions and the following disclaimer in the |
+ * |   documentation and/or other materials provided with the distribution.|
+ * | o The names of the authors may not be used to endorse or promote      |
+ * |   products derived from this software without specific prior written  |
+ * |   permission.                                                         |
+ * |                                                                       |
+ * | THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS   |
+ * | "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT     |
+ * | LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR |
+ * | A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT  |
+ * | OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, |
+ * | SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT      |
+ * | LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, |
+ * | DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY |
+ * | THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT   |
+ * | (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE |
+ * | OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  |
+ * |                                                                       |
+ * +-----------------------------------------------------------------------+
+ * | Author: Till Klampaeckel <till@php.net>                               |
+ * +-----------------------------------------------------------------------+
+ *
+ * PHP version 5
+ *
+ * @category Testing
+ * @package  Lagged_Test_PHPUnit_ControllerTestCase_Listener
+ * @author   Till Klampaeckel <till@php.net>
+ * @license  http://www.opensource.org/licenses/bsd-license.php The BSD License
+ * @version  GIT: $Id$
+ * @link     http://github.com/till/Lagged_Test_PHPUnit_ControllerTestCase_Listener
+ */
+
+/**
+ * @ignore
+ */
+require_once 'PHPUnit/Framework.php';
+
+/**
+ * A listener!
+ *
+ * @category Testing
+ * @package  Lagged_Test_PHPUnit_ControllerTestCase_Listener
+ * @author   Till Klampaeckel <till@php.net>
+ * @license  http://www.opensource.org/licenses/bsd-license.php The BSD License
+ * @version  Release: @package_version@
+ * @link     http://github.com/till/Lagged_Test_PHPUnit_ControllerTestCase_Listener
+ */
+class Lagged_Test_PHPUnit_ControllerTestCase_Listener implements PHPUnit_Framework_TestListener
+{
+    public function addError (
+        PHPUnit_Framework_Test $test,
+        Exception $e,
+        $time
+    ) {
+        printf("Error while running test '%s'.\n", $test->getName());
+    }
+ 
+    public function addFailure (
+        PHPUnit_Framework_Test $test,
+        PHPUnit_Framework_AssertionFailedError $e,
+        $time
+    ) {
+
+        printf("Test '%s' failed.\n", $test->getName());
+
+        echo "RESPONSE\n\n";
+
+        echo "Headers:\n\n";
+
+        foreach ($test->getResponse()->getHeaders() as $header) {
+            $replace = 'false';
+            if ($header['replace'] === true) {
+                $replace = 'true';
+            }
+            echo "\t {$header['name']} - {$header['value']} (replace: {$replace})\n";
+        }
+
+        echo "\n";
+
+        echo "Body:\n\n" . $test->getResponse()->getBody() . "\n\n";
+
+        if ($test->getResponse()->isException()) {
+
+            echo "Exceptions:\n\n";
+
+            foreach ($test->getResponse()->getException() as $exception) {
+
+                echo "\t * Message: {$exception->getMessage()}\n";
+                echo "\t * File:    {$exception->getFile()}\n";
+                echo "\t * Line:    {$exception->getLine()}\n";
+
+                echo "\n";
+            }
+        }
+    } 
+
+    public function addIncompleteTest (
+        PHPUnit_Framework_Test $test,
+        Exception $e,
+        $time
+    ) {
+        // do nada
+    }
+ 
+    public function addSkippedTest (
+        PHPUnit_Framework_Test $test,
+        Exception $e,
+        $time
+    ) {
+        // do nada
+    }
+ 
+    public function startTest(PHPUnit_Framework_Test $test)
+    {
+        // do nada
+    }
+ 
+    public function endTest(PHPUnit_Framework_Test $test, $time)
+    {
+    }
+ 
+    public function startTestSuite(PHPUnit_Framework_TestSuite $suite)
+    {
+        // do nada
+    }
+ 
+    public function endTestSuite(PHPUnit_Framework_TestSuite $suite)
+    {
+        // do nada
+    }
+}
